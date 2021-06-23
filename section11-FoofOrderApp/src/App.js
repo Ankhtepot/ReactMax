d09e03639bb6ea0dcfac86b452b71353bb2a0b2d
PetrZavodny <petr.zavodny@chyronhego.com>
2021-06-23 08:23:17 +0200

    Up to lesson 141.

---
+++ section11-FoofOrderApp/src/App.js
@@ -0,0 +1,28 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/CartItem/Cart";
+import {useState} from "react";
+
+function App() {
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    function showCartHandler() {
+        setCartIsShown(true);
+    }
+
+    function hideCartHandler() {
+        setCartIsShown(false);
+    }
+
+    return (
+        <>
+            {cartIsShown && <Cart onClose={hideCartHandler}/>}
+            <Header onShowCart={showCartHandler}/>
+            <main>
+                <Meals/>
+            </main>
+        </>
+    );
+}
+
+export default App;
