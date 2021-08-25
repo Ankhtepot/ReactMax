c8182df084ebe22243ff74de8421bbe38eabc44a
PetrZavodny <petr.zavodny@chyronhego.com>
2021-08-25 16:41:26 +0200

    Up to lesson 348.

--- section23-nextJS-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ section23-nextJS-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,13 +1,32 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
-import Layout from '../../components/layout/Layout';
+import {useRouter} from 'next/router';
+import Head from 'next/head';
 
 function NewMeetupPage() {
-    function addMeetupHandler(enteredMeetupData) {
-        console.log(enteredMeetupData);
+    const router = useRouter();
+
+    async function addMeetupHandler(enteredMeetupData) {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+        await router.push('/');
     }
 
     return (
         <>
+            <Head>
+                <title>Add a new meetup</title>
+                <meta name="description" content='Add your new meetup and create amazing networking opportunities.' />
+            </Head>
             <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </>
     );
